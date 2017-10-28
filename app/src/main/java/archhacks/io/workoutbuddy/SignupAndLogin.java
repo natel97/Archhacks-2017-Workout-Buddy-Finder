5ad6897ad3fe1d040e408ea07446c376f950233e
Nathanial Lubitz <nate.lubitz@gmail.com>
2017-10-28 10:14:05 -0500

    Filtered through images

--- app/src/main/java/archhacks/io/workoutbuddy/SignupAndLogin.java
+++ app/src/main/java/archhacks/io/workoutbuddy/SignupAndLogin.java
@@ -1,5 +1,6 @@
 package archhacks.io.workoutbuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,6 +16,7 @@ public class SignupAndLogin extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_login);
 
         Button logIn = (Button) findViewById(R.id.loginButton);
         Button signup = (Button) findViewById(R.id.signupButton);
@@ -24,7 +26,10 @@ public class SignupAndLogin extends AppCompatActivity{
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO Implement login
+                //TODO Implement login -- Temporarily (Plz undo later) auto moves to main
+                Intent i = new Intent();
+                i.setClass(getApplicationContext(),MainActivity.class);
+                startActivity(i);
             }
         });
 
