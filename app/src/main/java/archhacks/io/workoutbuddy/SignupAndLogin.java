4172460acfcb9a1fa25b659e80aea4d22ea13ab9
Nathanial Lubitz <nate.lubitz@gmail.com>
2017-10-28 02:10:38 -0500

    added some TODO's

---
+++ app/src/main/java/archhacks/io/workoutbuddy/SignupAndLogin.java
@@ -0,0 +1,31 @@
+package archhacks.io.workoutbuddy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by natel on 10/28/2017.
+ */
+
+public class SignupAndLogin extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        /* TODO: Create a layout in res/layout/signup_login.xml and set the ID's to be the same as the R.id.(...)
+        Commented out to prevent errors; uncomment when the below has been completed
+        Button logIn = (Button) findViewById(R.id.loginButton);
+        Button signup = (Button) findViewById(R.id.signupButton);
+        EditText email = (EditText) findViewById(R.id.emailInput);
+        EditText password = (EditText) findViewById(R.id.passwordInput);
+        */
+
+
+        
+
+
+    }
+}
